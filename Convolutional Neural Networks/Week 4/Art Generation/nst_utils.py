5a9161140532264c3d44190e6d82cfb8120fbfe7
Himanshu maurya <himanshumauryacrz@gmail.com>
2021-07-29 15:49:39 +0530

    Changed filter to filters in _conv2d()
    Does not give error TypeError: conv2d_v2() got an unexpected keyword argument 'filter' as mentioned in https://stackoverflow.com/questions/64033469/typeerror-conv2d-v2-got-an-unexpected-keyword-argument-filter

--- Convolutional Neural Networks/Week 4/Art Generation/nst_utils.py
+++ Convolutional Neural Networks/Week 4/Art Generation/nst_utils.py
@@ -108,7 +108,7 @@ def load_vgg_model(path):
         W, b = _weights(layer, layer_name)
         W = tf.constant(W)
         b = tf.constant(np.reshape(b, (b.size)))
-        return tf.nn.conv2d(prev_layer, filter=W, strides=[1, 1, 1, 1], padding='SAME') + b
+        return tf.nn.conv2d(prev_layer, filters=W, strides=[1, 1, 1, 1], padding='SAME') + b
 
     def _conv2d_relu(prev_layer, layer, layer_name):
         """
